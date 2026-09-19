90cfb7150eb42c97e7738bf5d5f5a17474a6e850
vmffpdlwnd <leegunehee@gmail.com>
2026-09-19 15:47:43 +0900

    Lv 2. SQL을 JPA 인덱스로 표현하기
    ChatMessage 파일 @Table 인덱스 선언

--- src/main/java/com/gameexpert/chat/entity/ChatMessage.java
+++ src/main/java/com/gameexpert/chat/entity/ChatMessage.java
@@ -2,27 +2,21 @@ package com.gameexpert.chat.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import com.gameexpert.world.entity.World;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
-// TODO Lv 2: 제공된 SQL과 같은 인덱스를 선언합니다.
-@Table(name = "chat_messages")
+// Lv 2 SQL 인덱스 설정
+@Table(name = "chat_messages", indexes = {
+        @Index(name = "idx_chat_world_created_at", columnList = "world_id, created_at")
+})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ChatMessage {
 
